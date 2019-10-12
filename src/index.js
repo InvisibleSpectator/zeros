f8e1a5661cfa76c37f7db0622e9c9a1fc9620a4d
InvisibleSpectator <vladislav5678@yandex.ru>
2019-10-13 01:40:46 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function zeros(expression) {
-  // your solution
+    if (expression.search(/[0,2,4,6,8]\!{2}|[0-9]\!(\*|$)/) == -1) {
+        return 0;
+    } else {
+        let arr = expression.split('*');
+        return arr.reduce((acc, value) => {
+            for (let i = Number(value.slice(0, value.indexOf('!'))); i > 0; i -= value.length - value.indexOf('!')) {
+                let temp = i;
+                while (temp % 5 == 0) {
+                    temp /= 5;
+                    acc++;
+                }
+            }
+            return acc;
+        }, 0);
+    }
 }
